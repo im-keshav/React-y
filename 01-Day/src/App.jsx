689db1b38a3ed20ab8de5126b479cd5eca957f7a
keshav <keshav@keshavs-MacBook-Air.local>
2025-10-28 16:28:42 +0530

    first commit

---
+++ 01-Day/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+   <>
+   <h1>Hello Everyone this is my First React Page</h1>
+   <h1>using fagment tag i can write multiple of code or tag bcz in return it only on thing 
+   </h1>
+   </>
+  )
+}
+
+export default App
